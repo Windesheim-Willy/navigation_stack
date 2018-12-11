d214cc222578c526e119bedbf0343fc9d168c05a
Dylan Reimerink <97.dylan@gmail.com>
2018-12-11 13:23:11 +0100

    Updated init script and fixed sim start script

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo apt-get install -y ros-kinetic-navigation-layers
+sudo apt-get install -y ros-kinetic-navigation-layers \
+ ros-kinetic-rosbag \
+ ros-kinetic-laser-scan-matcher \
+ ros-kinetic-move-base \
+ ros-kinetic-move-base-msgs \
+ ros-kinetic-map-server \
+ ros-kinetic-amcl \
+ ros-kinetic-hector-mapping 
--- start-sim-navstack.sh
+++ start-sim-navstack.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rosbag play bags/t5_lidar_and_cmdval.bag --delay=5 --clock &
-roslaunch navigation.launch sim:=true
+roslaunch navigation.launch sim:=true path:=$(pwd)
